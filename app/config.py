46893eb58898e9019e022247bebc6efe957e09a8
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2021-02-13 16:46:02 -0500

    add config file

---
+++ app/config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config(object):
+    """Base Config Object"""
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
+
+class DevelopmentConfig(Config):
+    """Development Config that extends the Base Config Object"""
+    DEVELOPMENT = True
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Production Config that extends the Base Config Object"""
+    DEBUG = False
