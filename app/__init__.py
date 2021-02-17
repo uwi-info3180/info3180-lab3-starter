a7da3ddd88cabec4e198a61913ffade1328376bb
Matthew Chevannes <lear.chevy@hotmail.com>
2021-02-17 15:46:46 -0500

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask_mail import Mail
+from .config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+mail = Mail(app)
 from app import views
--- app/config.py
+++ app/config.py
@@ -4,6 +4,10 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
+    MAIL_PORT = os.environ.get('MAIL_PORT') or '25'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD  = os.environ.get('MAIL_PASSWORD')
 
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
