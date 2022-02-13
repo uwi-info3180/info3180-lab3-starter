049bd54598efd3588c05f1ebb2d569637fc18666
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-02-13 11:07:42 -0500

    Simplify config and load .env file if present

--- app/config.py
+++ app/config.py
@@ -1,15 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-
-class DevelopmentConfig(Config):
-    """Development Config that extends the Base Config Object"""
-    DEVELOPMENT = True
-    DEBUG = True
-
-class ProductionConfig(Config):
-    """Production Config that extends the Base Config Object"""
-    DEBUG = False
