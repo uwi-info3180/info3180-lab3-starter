4d70e7016ba6682ee4e8dfd4657a6b009901755a
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2023-02-13 23:57:13 -0500

    Remove some code that is no longer necessary

--- app/views.py
+++ app/views.py
@@ -1,10 +1,3 @@
-"""
-Flask Documentation:     http://flask.pocoo.org/docs/
-Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
-Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
-This file creates your application.
-"""
-
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -63,7 +56,3 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="8080")
